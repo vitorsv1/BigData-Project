285622c4accb60d8459f1f98d6c42f06c4a90853
Lucas Vaz <lucastarraf@hotmail.com>
2019-10-10 18:56:13 -0300

    nomes coerentes

--- src/script_criacao.sql
+++ src/script_criacao.sql
@@ -1,7 +1,7 @@
 drop database if exists red_soc_passaros;
 create database if not exists red_soc_passaros;
-create table if not exists red_soc_passaros.usuarios (
-id int PRIMARY KEY auto_increment NOT NULL,
+create table if not exists red_soc_passaros.usuario (
+id_usuario int PRIMARY KEY auto_increment NOT NULL,
 nick varchar(32) UNIQUE,
 nome varchar(32),
 sobrenome varchar(32),
@@ -9,43 +9,43 @@ email varchar(32),
 cidade varchar(32)
 );
 
-create table if not exists red_soc_passaros.passaros (
-id int auto_increment NOT NULL,
+create table if not exists red_soc_passaros.passaro (
+id_passaro int auto_increment NOT NULL,
 especie varchar(32) UNIQUE KEY,
-PRIMARY KEY (id)
+PRIMARY KEY (id_passaro)
 );
 
 create table if not exists red_soc_passaros.preferencia (
-id_usuarios int NOT NULL,
-id_passaros int NOT NULL,
-PRIMARY KEY (id_usuarios, id_passaros),
-FOREIGN KEY (id_usuarios) REFERENCES usuarios(id),
-FOREIGN KEY (id_passaros) REFERENCES passaros(id)
+id_usuario int NOT NULL,
+id_passaro int NOT NULL,
+PRIMARY KEY (id_usuario, id_passaro),
+FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+FOREIGN KEY (id_passaro) REFERENCES passaros(id_passaro)
 );
 
 create table if not exists red_soc_passaros.post (
-id int PRIMARY KEY auto_increment NOT NULL,
+id_post int PRIMARY KEY auto_increment NOT NULL,
 id_usuario int,
 titulo varchar(32) NOT NULL,
 texto text,
 url varchar(32),
-foreign key (id_usuario) references usuarios(id)
+foreign key (id_usuario) references usuario(id_usuario)
 );
 
 create table if not exists red_soc_passaros.mençao (
 id_post int,
 id_usuario int,
 PRIMARY KEY (id_post, id_usuario),
-FOREIGN KEY (id_post) REFERENCES post(id),
-FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+FOREIGN KEY (id_post) REFERENCES post(id_post),
+FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
 
 create table if not exists red_soc_passaros.marca_passaro (
 id_passaro int,
 id_post int,
 primary key (id_passaro, id_post),
-FOREIGN KEY (id_post) REFERENCES post(id),
-FOREIGN KEY (id_passaro) REFERENCES passaros(id)
+FOREIGN KEY (id_post) REFERENCES post(id_post),
+FOREIGN KEY (id_passaro) REFERENCES passaro(id_passaro)
 );
 
 create table if not exists red_soc_passaros.visualizaçao (
@@ -56,6 +56,6 @@ ip varchar(32),
 browser varchar(32),
 data_visualizaçao timestamp,
 PRIMARY KEY (id_post, id_usuario),
-FOREIGN KEY (id_post) REFERENCES post(id),
-FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+FOREIGN KEY (id_post) REFERENCES post(id_post),
+FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
