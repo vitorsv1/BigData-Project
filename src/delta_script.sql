9fb7f6c7105633d64868d81fecb6534db616ba40
vitorsv1 <vitorsv1@al.insper.edu.br>
2019-10-21 00:24:39 -0300

    Script Delta

--- src/delta_script.sql
+++ src/delta_script.sql
@@ -3,9 +3,12 @@ USE red_soc_passaros;
 ALTER TABLE post
 MODIFY COLUMN  url varchar(1000);
 
+ALTER TABLE visualizacao DROP FOREIGN KEY visualizacao_ibfk_1;
+ALTER TABLE visualizacao DROP FOREIGN KEY visualizacao_ibfk_2; 
 ALTER TABLE visualizacao
 DROP PRIMARY KEY;
-ADD id_visualizacao int PRIMARY KEY auto_increment NOT NULL;
+ALTER TABLE visualizacao
+add id_visualizacao int primary key auto_increment;
 
 
 CREATE TABLE IF NOT EXISTS lugar (
@@ -20,4 +23,13 @@ id_post int,
 primary key (id_lugar, id_post),
 FOREIGN KEY (id_post) REFERENCES post(id_post),
 FOREIGN KEY (id_lugar) REFERENCES lugar(id_lugar)
+);
+
+CREATE TABLE IF NOT EXISTS usuario_post_like (
+id_post int,
+id_usuario int,
+post_like varchar(32),
+PRIMARY KEY (id_post, id_usuario),
+FOREIGN KEY (id_post) REFERENCES post(id_post),
+FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
